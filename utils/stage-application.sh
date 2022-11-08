6adbe5ddc55d90b1e0fe9a859a42bea78fb3184c
Hossein Kassaei <hossein.kassaei@gmail.com>
2022-11-08 13:22:44 -0500

    utility script to facilitate deployin the application

---
+++ utils/stage-application.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+cp ../application/blueprint/kustomization.yaml ../application/deployment
+cp ../application/blueprint/edc-demo-app.yaml ../application/deployment
+cp ../application/blueprint/service_account.yaml ../application/deployment
+cp ../application/blueprint/gcp-cloudsql-single-claim.yaml ../application/deployment
